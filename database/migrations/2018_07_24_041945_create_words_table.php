ac2f44e6ee68a3da5ea8580bb0752735bc3a9f7d
ryukam10k <r.kamioka@gmail.com>
2018-07-24 13:44:31 +0900

    add migrate.

---
+++ database/migrations/2018_07_24_041945_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('howtoread');
+            $table->string('meaning');
+            $table->string('source');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
