f7248301bd859724913385294c011585f2307f3f
ryukam10k <r.kamioka@gmail.com>
2018-07-29 17:18:14 +0900

    Add Index Action For Word

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Word;
 
 class WordController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $items = Word::all();
+        return view('word.index', ['items' => $items]);
+    }
 }
---
+++ resources/views/word/index.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Words | Growords</title>
+<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+    <h1>Words</h1>
+    <table class="table">
+        <tr>
+            <th>Name</th>
+            <th>Meaning</th>
+            <th></th>
+        </tr>
+        @foreach ($items as $item)
+        <tr>
+            <td>{{$item->name}}</td>
+            <td>{{$item->meaning}}</td>
+            <td>
+                <a href="/word/show?id={{$item->id}}">show</a> | 
+                <a href="/word/edit?id={{$item->id}}">edit</a> | 
+                <a href="/word/del?id={{$item->id}}">del</a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
