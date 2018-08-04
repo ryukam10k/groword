bc9cc9c5c2988c8c6a52e6a0e0842148506933e2
ryukam10k <r.kamioka@gmail.com>
2018-08-04 22:56:30 +0900

    #1 Use layouts.app With Word

--- resources/views/word/add.blade.php
+++ resources/views/word/add.blade.php
@@ -1,43 +1,49 @@
-<html>
-<head>
-<title>Add | Words | Growords</title>
-<link rel="stylesheet" type="text/css" href="/css/app.css">
-</head>
-<body>
-    <h1>Add - Words</h1>
-    @if (count($errors) > 0)
-    <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="card">
+                <div class="card-header">Words - Add</div>
+                <div class="card-body">
+                    @if (count($errors) > 0)
+                    <div>
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+                    <form action="/word/add" method="post">
+                    <table class="table">
+                        {{ csrf_field() }}
+                        <tr>
+                            <th>name: </th>
+                            <td><input type="text" name="name" value="{{old('name')}}"></td>
+                        </tr>
+                        <tr>
+                            <th>howtoread: </th>
+                            <td><input type="text" name="howtoread" vaule="{{old('howtoread')}}"></td>
+                        </tr>
+                        <tr>
+                            <th>meaning: </th>
+                            <td><input type="text" name="meaning" value="{{old('meaning')}}"></td>
+                        </tr>
+                        <tr>
+                            <th>source: </th>
+                            <td><input type="text" name="source" value="{{old('source')}}"></td>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <td><input type="submit" value="send"></td>
+                        </tr>
+                    </table>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-    @endif
-    <table class="table">
-    <form action="/word/add" method="post">
-        {{ csrf_field() }}
-        <tr>
-            <th>name: </th>
-            <td><input type="text" name="name" value="{{old('name')}}"></td>
-        </tr>
-        <tr>
-            <th>howtoread: </th>
-            <td><input type="text" name="howtoread" vaule="{{old('howtoread')}}"></td>
-        </tr>
-        <tr>
-            <th>meaning: </th>
-            <td><input type="text" name="meaning" value="{{old('meaning')}}"></td>
-        </tr>
-        <tr>
-            <th>source: </th>
-            <td><input type="text" name="source" value="{{old('source')}}"></td>
-        </tr>
-        <tr>
-            <th></th>
-            <td><input type="submit" value="send"></td>
-        </tr>
-    </form>
-    </table>
-</body>
-</html>
+</div>
+@endsection
--- resources/views/word/index.blade.php
+++ resources/views/word/index.blade.php
@@ -1,28 +1,34 @@
-<html>
-<head>
-<title>Words | Growords</title>
-<link rel="stylesheet" type="text/css" href="/css/app.css">
-</head>
-<body>
-    <h1>Words</h1>
-    <div><a href="/word/add">Add</a></div>
-    <table class="table">
-        <tr>
-            <th>Name</th>
-            <th>Meaning</th>
-            <th></th>
-        </tr>
-        @foreach ($items as $item)
-        <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->meaning}}</td>
-            <td>
-                <a href="/word/show?id={{$item->id}}">show</a> | 
-                <a href="/word/edit?id={{$item->id}}">edit</a> | 
-                <a href="/word/del?id={{$item->id}}">del</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">Words</div>
+                <div class="card-body">
+                    <div><a href="/word/add">Add</a></div>
+                    <table class="table">
+                        <tr>
+                            <th>Name</th>
+                            <th>Meaning</th>
+                            <th></th>
+                        </tr>
+                        @foreach ($items as $item)
+                        <tr>
+                            <td>{{$item->name}}</td>
+                            <td>{{$item->meaning}}</td>
+                            <td>
+                                <a href="/word/show?id={{$item->id}}">show</a> | 
+                                <a href="/word/edit?id={{$item->id}}">edit</a> | 
+                                <a href="/word/del?id={{$item->id}}">del</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
