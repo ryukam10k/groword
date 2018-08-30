53fc545fd2b95fb745e7904e3ffcd9ea5d213713
rkamioka <kamioka_ryuya@kanso.co.jp>
2018-08-30 12:43:13 +0900

    Fix Migrations #6

--- database/migrations/2018_08_28_075019_create_tagmap_table.php
+++ database/migrations/2018_08_28_075019_create_tagmap_table.php
@@ -13,7 +13,7 @@ class CreateTagmapTable extends Migration
      */
     public function up()
     {
-        Schema::create('tagmap', function (Blueprint $table) {
+        Schema::create('tagmaps', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('word_id');
             $table->integer('tag_id');
