9239d17cc23182289a71e54704cb3be3bf526b20
rkamioka <kamioka_ryuya@kanso.co.jp>
2018-08-30 13:10:51 +0900

    Fix Seeder files #6

--- database/seeds/TagTableSeeder.php
+++ database/seeds/TagTableSeeder.php
@@ -11,6 +11,30 @@ class TagTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('tags')->insert([
+            [
+                'name' => 'IT用語',
+            ],
+            [
+                'name' => '法律用語',
+            ],
+            [
+                'name' => '名言',
+            ]
+        ]);
+        DB::table('tagmaps')->insert([
+            [
+                'word_id' => 1,
+                'tag_id' => 1
+            ],
+            [
+                'word_id' => 2,
+                'tag_id' => 2
+            ],
+            [
+                'word_id' => 2,
+                'tag_id' => 3
+            ]
+        ]);
     }
 }
