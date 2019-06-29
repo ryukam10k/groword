089a2348966f19528db28858f553c6b9638e408b
ryukam10k <r.kamioka@gmail.com>
2019-06-29 19:46:55 +0900

    Fix Layouts

--- resources/views/sentence/add.blade.php
+++ resources/views/sentence/add.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
+    <div class="row">
+        <div class="col-12 col-md-8">
             <div class="card">
                 <div class="card-header">
                     <div class="content__header__title"><a href="/sentence/">English Sentence</a> - Add</div>
@@ -19,35 +19,35 @@
                     </div>
                     @endif
                     <form action="/sentence/add" method="post">
-                    <table class="table">
                         {{ csrf_field() }}
-                        <tr>
-                            <th>sentence: </th>
-                            <td><textarea v-on:change="getWords" id="sentence" v-model="sentence" class="form-control" rows="4" name="name">{{old('name')}}</textarea></td>
-                        </tr>
-                        <tr>
-                            <th>meaning: </th>
-                            <td><textarea class="form-control" rows="4" name="meaning">{{old('meaning')}}</textarea></td>
-                        </tr>
-                        <tr>
-                            <th></th>
-                            <td><input type="submit" class="btn btn-primary" value="send"></td>
-                        </tr>
-                    </table>
+                        <div>Sentence</div>
+                        <div>
+                            <textarea v-on:change="getWords" id="sentence" v-model="sentence" class="form-control" rows="4" name="name">{{old('name')}}</textarea>
+                        </div>
+                        <div>Meaning</div>
+                        <div>
+                            <textarea class="form-control" rows="4" name="meaning">{{old('meaning')}}</textarea>
+                        </div>
+                        <hr>
+                        <div>
+                            <input type="submit" class="btn btn-primary btn-block" value="Save">
+                        </div>
                     </form>
                 </div>
-                <div class="card">
-                    <div class="card-header">
-                        <div class="content__header__title">Related words</div>
-                    </div>
-                    <div class="card-body">
-                        <ul>
-                            <li v-for="word in words">
-                                @{{ word.name + "   " + word.meaning }}
-                                <a v-if="word.meaning === 'unknown'" href="/eng_word/add" target="_blank">:Add</a>
-                            </li>
-                        </ul>
-                    </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <div class="content__header__title">Related words</div>
+                </div>
+                <div class="card-body">
+                    <ul>
+                        <li v-for="word in words">
+                            @{{ word.name + "   " + word.meaning }}
+                            <a v-if="word.meaning === 'unknown'" href="/eng_word/add" target="_blank">:Add</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
--- resources/views/sentence/edit.blade.php
+++ resources/views/sentence/edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-lg-8">
+        <div class="col-12 col-md-8">
             <div class="card">
                 <div class="card-header">
                     <div class="content__header__title"><a href="/sentence/">English Sentence</a> - Edit</div>
@@ -19,25 +19,25 @@
                     </div>
                     @endif
                     <form action="/sentence/edit" method="post">
-                    {{ csrf_field() }}
-                    <input type="hidden" name="id" value="{{$form->id}}">
-                    <table class="table">
-                        <tr>
-                            <th>sentence: </th>
-                            <td><textarea v-on:change="getWords" id="sentence" v-model="sentence" class="form-control" rows="4" name="name">{{$form->name}}</textarea></td>
-                        </tr>
-                        <tr>
-                            <th>meaning: </th>
-                            <td><textarea class="form-control" rows="4" name="meaning">{{$form->meaning}}</textarea></td>
-                        </tr>
-                        <tr>
-                            <th></th>
-                            <td><input type="submit" class="btn btn-primary" value="send"></td>
-                        </tr>
-                    </table>
+                        {{ csrf_field() }}
+                        <input type="hidden" name="id" value="{{$form->id}}">
+                        <div>Sentence</div>
+                        <div>
+                            <textarea v-on:change="getWords" id="sentence" v-model="sentence" class="form-control" rows="4" name="name">{{$form->name}}</textarea>
+                        </div>
+                        <div>Meaning</div>
+                        <div>
+                            <textarea class="form-control" rows="4" name="meaning">{{$form->meaning}}</textarea>
+                        </div>
+                        <hr>
+                        <div>
+                            <input type="submit" class="btn btn-primary btn-block" value="Save">
+                        </div>
                     </form>
                 </div>
             </div>
+        </div>
+        <div class="col">
             <div class="card">
                 <div class="card-header">
                     <div class="content__header__title">Related words</div>
